cb77642423496ec28c8f153032ca79279ef85d25
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2019-11-08 12:05:59 +0100

    First commit. Screen MainPage in Developement

---
+++ PathfindersClubApp/PathfindersClubApp.Android/obj/Debug/90/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 91c4fc5a1c369911b9bec3aa925c59d37e3bf6a2 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1486
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 389333
+	.include	"typemap.mj.inc"
